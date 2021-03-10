392a690fcdc29d78677764cf2c0b29e620edb1f9
maki <maki@void.com>
2021-03-10 21:36:44 +1000

    > added theme template and first theme

--- themes.h
+++ themes.h
@@ -1,1 +1,29 @@
 /* theme template */
+
+// static const char *fonts[]		= { "monospace:size=10" };
+// static const char dmenufont[]	= "monospace:size=10" };
+//
+// static const char col_mbar0[]	= "#000000";
+// static const char col_mbar1[]	= "#000000";
+//
+// static const char col_brdr0[]	= "#000000";
+// static const char col_brdr1[]	= "#000000";
+//
+// static const char col_text0[]	= "#000000";
+// static const char col_text1[]	= "#000000";
+
+
+
+/* maki */
+
+static const char *fonts[]	= { "monospace:size=10" };
+static const char dmenufont[]	= "monospace:size=10" };
+
+static const char col_mbar0[]	= "#2b2b2b";
+static const char col_mbar1[]	= "#f6f6f6";
+
+static const char col_brdr0[]	= "#f6f6f6";
+static const char col_brdr1[]	= "#9cdbc9";
+
+static const char col_text0[]	= "#f6f6f6";
+static const char col_text1[]	= "#2b2b2b";
