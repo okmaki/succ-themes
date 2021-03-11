c985ac1c86700cc438004ba285238b9bfd255a0a
maki <maki@void.com>
2021-03-11 22:51:11 +1000

    > updated theme

--- themes.h
+++ themes.h
@@ -19,11 +19,11 @@
 static const char *fonts[]	= { "monospace:size=10" };
 static const char dmenufont[]	= "monospace:size=10";
 
-static const char col_mbar0[]	= "#2b2b2b";
-static const char col_mbar1[]	= "#f6f6f6";
+static const char col_mbar0[]	= "#2e3440";
+static const char col_mbar1[]	= "#d8dee9";
 
 static const char col_brdr0[]	= "#f6f6f6";
 static const char col_brdr1[]	= "#9cdbc9";
 
-static const char col_text0[]	= "#f6f6f6";
-static const char col_text1[]	= "#2b2b2b";
+static const char col_text0[]	= "#d8dee9";
+static const char col_text1[]	= "#2e3440";
