f8efbf8e68f9be76d516dc58e4a842396a7e53ba
maki <maki@void.com>
2021-03-15 16:14:34 +1000

    > added 2 new themes

--- themes.h
+++ themes.h
@@ -1,36 +1,89 @@
 static const char *fonts[]	= { "White Rabbit:size=10" };
 static const char dmenufont[]	= "White Rabbit:size=10";
 
-static const char col_mbar0[]	= "#2e3440";
-static const char col_mbar1[]	= "#d8dee9";
-
-static const char col_brdr0[]	= "#f6f6f6";
-static const char col_brdr1[]	= "#9cdbc9";
-
-static const char col_text0[]	= "#d8dee9";
-static const char col_text1[]	= "#2e3440";
+// ------------------------------
+// norde 
+// ------------------------------
 
 /* 8 normal colors */
-static const char col_black0[]	= "#3b4252";
-static const char col_red0[]	= "#bf616a"; 
-static const char col_green0[]	= "#a3be8c";
-static const char col_yellow0[]	= "#ebcb8b";
-static const char col_blue0[]	= "#81a1c1";
-static const char col_magenta0[]= "#b48ead";
-static const char col_cyan0[]	= "#88c0d0";
-static const char col_white0[]	= "#e5e9f0";
+//static const char col_black0[]	= "#3b4252";
+//static const char col_red0[]	= "#bf616a"; 
+//static const char col_green0[]	= "#a3be8c";
+//static const char col_yellow0[]	= "#ebcb8b";
+//static const char col_blue0[]	= "#81a1c1";
+//static const char col_magenta0[]= "#b48ead";
+//static const char col_cyan0[]	= "#88c0d0";
+//static const char col_white0[]	= "#e5e9f0";
+//
+///* 8 bright colors */
+//static const char col_black1[]  = "#4c566a";
+//static const char col_red1[]    = "#bf616a";
+//static const char col_green1[]  = "#a3be8c";
+//static const char col_yellow1[] = "#ebcb8b";
+//static const char col_blue1[]   = "#81a1c1";
+//static const char col_magenta1[]= "#b48ead";
+//static const char col_cyan1[]   = "#8fbcbb";
+//static const char col_white1[]  = "#eceff4";
+//
+///* more colors can be added after 255 to use with DefaultXX */
+//static const char col_bg[]	= "#2e3440";
+//static const char col_fg[]	= "#d8dee9";
+//static const char col_00[]	= "#000000";
+
+// ------------------------------
+// cyberpunk 
+// ------------------------------
+
+/* 8 normal colors */
+//static const char col_black0[]  = "#000000";
+//static const char col_red0[]    = "#ff7092";
+//static const char col_green0[]  = "#00fbac";
+//static const char col_yellow0[] = "#fffa6a";
+//static const char col_blue0[]   = "#00bfff";
+//static const char col_magenta0[]= "#df95ff";
+//static const char col_cyan0[]   = "#86cbfe";
+//static const char col_white0[]  = "#ffffff";
+//
+///* 8 bright colors */
+//static const char col_black1[]  = "#000000";
+//static const char col_red1[]    = "#ff8aa4";
+//static const char col_green1[]  = "#21f6bc";
+//static const char col_yellow1[] = "#fff787";
+//static const char col_blue1[]   = "#1bccfd";
+//static const char col_magenta1[]= "#e6aefe";
+//static const char col_cyan1[]   = "#99d6fc";
+//static const char col_white1[]  = "#ffffff";
+//
+///* more colors can be added after 255 to use with DefaultXX */
+//static const char col_bg[]      = "#332a57";
+//static const char col_fg[]      = "#e5e5e5";
+//static const char col_00[]      = "#000000";
+
+// ------------------------------
+// monokai 
+// ------------------------------
+
+/* 8 normal colors */
+static const char col_black0[]  = "#1f1f1f";
+static const char col_red0[]    = "#f92672";
+static const char col_green0[]  = "#a6e22e";
+static const char col_yellow0[] = "#e6db74";
+static const char col_blue0[]   = "#6699df";
+static const char col_magenta0[]= "#ae81ff";
+static const char col_cyan0[]   = "#e69f66";
+static const char col_white0[]  = "#f8f8f2";
 
 /* 8 bright colors */
-static const char col_black1[]  = "#4c566a";
-static const char col_red1[]    = "#bf616a";
-static const char col_green1[]  = "#a3be8c";
-static const char col_yellow1[] = "#ebcb8b";
-static const char col_blue1[]   = "#81a1c1";
-static const char col_magenta1[]= "#b48ead";
-static const char col_cyan1[]   = "#8fbcbb";
-static const char col_white1[]  = "#eceff4";
+static const char col_black1[]  = "#75715e";
+static const char col_red1[]    = "#f92672";
+static const char col_green1[]  = "#a6e22e";
+static const char col_yellow1[] = "#e6db74";
+static const char col_blue1[]   = "#66d9ef";
+static const char col_magenta1[]= "#ae81ff";
+static const char col_cyan1[]   = "#e69f66";
+static const char col_white1[]  = "#f8f8f2";
 
 /* more colors can be added after 255 to use with DefaultXX */
-static const char col_bg[]	= "#2e3440";
-static const char col_fg[]	= "#d8dee9";
-static const char col_00[]	= "#000000";
+static const char col_bg[]      = "#1f1f1f";
+static const char col_fg[]      = "#f8f8f8";
+static const char col_00[]      = "#000000";
