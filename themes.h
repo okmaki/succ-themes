be2d929de5b196a4e732075047ec33b9b5cebaa8
maki <maki@void.com>
2021-03-15 10:41:05 +1000

    . updated fonts

--- themes.h
+++ themes.h
@@ -1,23 +1,5 @@
-/* theme template */
-
-// static const char *fonts[]		= { "monospace:size=10" };
-// static const char dmenufont[]	= "monospace:size=10";
-//
-// static const char col_mbar0[]	= "#000000";
-// static const char col_mbar1[]	= "#000000";
-//
-// static const char col_brdr0[]	= "#000000";
-// static const char col_brdr1[]	= "#000000";
-//
-// static const char col_text0[]	= "#000000";
-// static const char col_text1[]	= "#000000";
-
-
-
-/* maki */
-
-static const char *fonts[]	= { "monospace:size=10" };
-static const char dmenufont[]	= "monospace:size=10";
+static const char *fonts[]	= { "White Rabbit:size=10" };
+static const char dmenufont[]	= "White Rabbit:size=10";
 
 static const char col_mbar0[]	= "#2e3440";
 static const char col_mbar1[]	= "#d8dee9";
@@ -27,3 +9,28 @@ static const char col_brdr1[]	= "#9cdbc9";
 
 static const char col_text0[]	= "#d8dee9";
 static const char col_text1[]	= "#2e3440";
+
+/* 8 normal colors */
+static const char col_black0[]	= "#3b4252";
+static const char col_red0[]	= "#bf616a"; 
+static const char col_green0[]	= "#a3be8c";
+static const char col_yellow0[]	= "#ebcb8b";
+static const char col_blue0[]	= "#81a1c1";
+static const char col_magenta0[]= "#b48ead";
+static const char col_cyan0[]	= "#88c0d0";
+static const char col_white0[]	= "#e5e9f0";
+
+/* 8 bright colors */
+static const char col_black1[]  = "#4c566a";
+static const char col_red1[]    = "#bf616a";
+static const char col_green1[]  = "#a3be8c";
+static const char col_yellow1[] = "#ebcb8b";
+static const char col_blue1[]   = "#81a1c1";
+static const char col_magenta1[]= "#b48ead";
+static const char col_cyan1[]   = "#8fbcbb";
+static const char col_white1[]  = "#eceff4";
+
+/* more colors can be added after 255 to use with DefaultXX */
+static const char col_bg[]	= "#2e3440";
+static const char col_fg[]	= "#d8dee9";
+static const char col_00[]	= "#000000";
